6d493fee1705ab54452afe9e077120a0a9be6a41
Lucas da Mata <lucas.mata@b2list.com>
2021-09-02 22:25:26 -0300

    Componentization

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,11 +3,12 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
   :root {
     --background: #f0f2f5;
+    --green: #33cc95;
     --red: #e52e4d;
     --blue: #5429cc;
     --blue-light: #6933ff;
     --text-title: #363f5f;
-    --text-body: #f0f2f5;
+    --text-body: #969CB3;
     --shape: #ffffff;
   }
   
@@ -28,7 +29,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background: var(---background);
+    background: var(--background);
     -webkit-font-smoothing: antialiased;
   }
 
